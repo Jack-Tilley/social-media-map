427ee01f2d9cef788d4e0c730cea8ee84b1cde59
Jack-Tilley <tilley.e.jack@gmail.com>
2021-01-08 15:36:22 -0500

    started working on models

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,6 +4,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    communities = models.ManyToManyField(
+        'Community', blank=True, related_name="communities")
+    score = models.OneToOneField('Score', blank=True, related_name="score")
+
+
+class Posts(models.Model):
+    pass
+
+
 class Comment(models.Model):
     content = models.CharField(max_length=256, blank=False, null=False)
     created = models.DateTimeField(auto_now=True)
