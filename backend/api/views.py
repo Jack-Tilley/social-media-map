0f30185f68580c7664c837916fee4552207645ca
Jack-Tilley <tilley.e.jack@gmail.com>
2021-01-08 14:30:39 -0500

    updated boilerplate

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+# from .serializers import *
+# from .models import *
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from django.contrib.auth.models import User
 
 # Create your views here.
